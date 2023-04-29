0a2feb798e429bfdf3c98f04feaaacdf6303adb4
shalini-peeka <120364061+shalini-peeka@users.noreply.github.com>
2023-04-29 11:32:48 +0530

    added in main test

--- src/test/java/com/ebookExample/ebookExample/EbookExampleApplicationTests.java
+++ src/test/java/com/ebookExample/ebookExample/EbookExampleApplicationTests.java
@@ -3,7 +3,7 @@ package com.ebookExample.ebookExample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EbookExampleApplication.class)
 class EbookExampleApplicationTests {
 
 	@Test
